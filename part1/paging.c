7b902377b29f13f162baf038f1f1934aa78577d3
EricShanks68 <43218234+EricShanks68@users.noreply.github.com>
2022-04-07 16:17:11 -0700

    Update paging.c
    Added comments

--- part1/paging.c
+++ part1/paging.c
@@ -3,15 +3,18 @@
 #include <stdlib.h> 
 #include <time.h>
 
+//compute the page number and offset
 void get_offset_and_page( int offset, int page_number, unsigned int virtual_address, int virtual_adress_i ) {
 
 
     int i = virtual_adress_i - 1;
+    //mask for both page and offset act as a kind of "offset"
     int page_mask = 65535; //65535
     int offset_mask = 255; //255
 
     int ex_off; int ex_page;
-
+    
+    //calculate page number and offset number
     ex_page = (virtual_address & page_mask) >> page_number;
     ex_off = (virtual_address & offset_mask) ;
     printf("virtual address %d is in page: %d and offset: %d \n", i, ex_page, ex_off);
@@ -33,7 +36,8 @@ int m;
 
 //16bit unsigned between 256- 65535
 unsigned int addr = 2356;
-
+    
+    //read file line by line where first 2 lines are page and offset bits, and rest are virtual addresses
     while (fgets(output, sizeof(output), file) !=NULL) {
 
         // printf("%d\n", atoi(output));
